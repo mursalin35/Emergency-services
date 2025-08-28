1625e45377f0d4e18d508a2f08a6e818c8a9ee7e
mursalin35 <msmursalin35@gmail.com>
2025-08-28 08:44:12 +0600

    alert massage add

---
+++ jsvascrip.js
@@ -0,0 +1,44 @@
+// Heart count function 
+let heartCount = 0;
+function increaseHeart(){
+    heartCount++;
+    document.getElementById('heratCounter').innerText = heartCount;
+}
+
+// Alert massage 
+// card no 1 
+function emergency(){
+    alert( "📞 Calling National Emergency 999...");
+}
+// card no 2 
+function police(){
+    alert( "📞 Calling Police 999...");
+}
+// card no 3 
+function fire(){
+    alert( "📞 Calling Fire Service 999...");
+}
+// card no 4 
+function ambulance(){
+    alert( "📞 Calling Ambulance 1994-999999...");
+}
+// card no 5 
+function women(){
+    alert( "📞 Calling Women & Child Helpline 109...");
+}
+// card no 6 
+function corruption(){
+    alert( "📞 Calling Anti-Corruption 106...");
+}
+// card no 7 
+function electricity(){
+    alert( "📞 Calling Electricity Outage 16216...");
+}
+// card no 8 
+function brac(){
+    alert( "📞 Calling Brac 16445...");
+}
+// card no 9 
+function railway(){
+    alert( "📞 Calling Bangladesh Railway 163...");
+}
